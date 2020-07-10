90a45834a0beef306340097cc04bbd6b77e34f66
Arjie Redona <53202848+Aramxv@users.noreply.github.com>
2020-07-10 12:08:00 +0800

    Add files via upload

---
+++ StringSortWPF/WpfApp1/Controller/SortControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+using StringSorter.View;
+using StringSorter.Model;
+using StringSorter.Interface;
+
+namespace StringSorter.Controller
+{
+    public class SortControl 
+    {
+        private View.SorterMainWindow _main = new SorterMainWindow();
+        
+        public void SortStringNow()
+        {
+            try
+            {
+                _main.SortedString = _main._sorter.Sort((ISorter)_main.cmbAlgo.SelectedItem, _main.txtInput.Text.Trim());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ StringSortWPF/WpfApp1/Model/Sorter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using StringSorter.Interface;
+
+namespace StringSorter.Model
+{
+    public class Sorter
+    {
+        public string Sort(ISorter sorter, string s)
+        {
+            return sorter.Sort(s);
+        }
+
+        public IEnumerable<ISorter> GetSorters()
+        {
+            List<ISorter> sorters = new List<ISorter>();
+            sorters.Add(new BubbleSorter());
+            sorters.Add(new MergeSorter());
+            return sorters;
+        }
+    }
+}
